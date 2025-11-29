a0e277a90f6ca263c7c59559a0d468d69075cc47
randy-aa <randyakiva0@gmail.com>
2025-11-29 09:12:36 +0300

    Exercise1-Arrays.py

--- Exercise1-Arrays.py
+++ Exercise1-Arrays.py
@@ -1,22 +1,21 @@
-import numpy as np
-
-transactions = np.array([
-    [1200, 1350, 1100, 980, 1500, 1600],
-    [900, 1050, 1150, 1200, 1300, 1250],   
-    [2000, 2100, 1950, 2200, 2050, 2150],  
-    [800, 950, 1000, 1100, 1200, 1150]     
+import numpy as np 
+sales_data = np.array([
+    [450, 520, 480, 500],
+    [600, 580, 620, 610],
+    [700, 750, 720, 740],
+    [300, 350, 320, 310],
+    [800, 820, 780, 810]
 ])
 
-totals = transactions.sum(axis=1)
+store_totals = sales_data.sum(axis=1)
 
-highest_branch = np.argmax(totals) + 1  
+top_store = np.argmax(store_totals) + 1  
+overall_avg = sales_data.mean()
 
-avg_monthly = transactions.mean()
+reshaped_sales = sales_data.reshape(2, 10)
 
-reshaped = transactions.reshape(3, 8)
-
-print("Transactions:\n", transactions)
-print("Total per branch:", totals)
-print("Highest branch:", highest_branch)
-print("Average monthly volume:", avg_monthly)
-print("Reshaped (3x8):\n", reshaped)
+print("Sales Data:\n", sales_data)
+print("Total per store:", store_totals)
+print("Top performing store:", top_store)
+print("Overall average sales:", round(overall_avg, 2))
+print("Reshaped (2x10):\n", reshaped_sales)
